c6f431da598bf5f0ddef2746bcd634e6a5d5f1a7
cjeongmin <cjeongmin27@gmail.com>
2025-07-22 23:30:30 +0900

    chore: Initialize Schemafy Server project with Gradle

---
+++ apps/server/src/main/java/com/schemafy/server/SchemafyServerApplication.java
@@ -0,0 +1,13 @@
+package com.schemafy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemafyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchemafyServerApplication.class, args);
+    }
+
+}
---
+++ apps/server/src/test/java/com/schemafy/server/SchemafyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schemafy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemafyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
