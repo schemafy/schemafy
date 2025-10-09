e6d39d5afab4858583546f7b4e7d75bcaaa21fda
he2e2 <dddd6368@naver.com>
2025-10-09 21:33:18 +0900

    feat(#54): add Avatar component

--- apps/frontend/src/components/Header/Contents.tsx
+++ apps/frontend/src/components/Header/Contents.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar';
 import { Button } from '../Button';
 
 export const LandingContents = () => {
@@ -28,9 +29,10 @@ export const DashBoardContents = () => {
       <div className="flex gap-2">
         <Button round>New Project</Button>
         <Button variant={'secondary'} round to="/signin">
-          Sign In
+          Sign Out
         </Button>
       </div>
+      <Avatar src="https://picsum.photos/200/300?random=1" />
     </div>
   );
 };
@@ -56,6 +58,14 @@ export const CanvasContents = () => {
       <Button variant={'secondary'} round>
         Sign Out
       </Button>
+      <div className="flex items-center gap-2">
+        <div className="flex items-center -space-x-3 *:data-[slot=avatar]:ring-background *:data-[slot=avatar]:ring-2 [&>*:nth-child(1)]:z-30 [&>*:nth-child(2)]:z-20 [&>*:nth-child(3)]:z-10">
+          <Avatar src="https://picsum.photos/200/300?random=1" />
+          <Avatar src="https://picsum.photos/100/300?random=1" deactivate />
+          <Avatar src="https://picsum.photos/200/100?random=1" deactivate />
+        </div>
+        <span className="font-overline-xs text-schemafy-dark-gray">+ 3</span>
+      </div>
     </div>
   );
 };
