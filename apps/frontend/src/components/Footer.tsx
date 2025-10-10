078e67507bc65a5bb82ead9bf8bd079dba86e23a
Cho-heejung <66050038+he2e2@users.noreply.github.com>
2025-10-10 17:39:58 +0900

    fix(#54): Fixed Header, Footer (#65)
    * fix(#54): fixed layout's width
    
    * fix(#54): fixed footer contents
    
    * fix(#54): fixed header contents
    
    * feat(#54): add Avatar component

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -1,39 +1,9 @@
-import { Button } from './Button';
-
 export const Footer = () => {
   return (
-    <footer className="w-full flex justify-center">
-      <div className="flex max-w-[960px] flex-col items-start grow py-10 px-5 gap-6 self-stretch">
-        <div className="self-stretch flex justify-between">
-          <Button
-            variant={'none'}
-            className="text-schemafy-dark-gray min-w-[10rem] text-[1rem]"
-          >
-            Product
-          </Button>
-          <Button
-            variant={'none'}
-            className="text-schemafy-dark-gray min-w-[10rem] text-[1rem]"
-          >
-            Pricing
-          </Button>
-          <Button
-            variant={'none'}
-            className="text-schemafy-dark-gray min-w-[10rem] text-[1rem]"
-          >
-            Resources
-          </Button>
-          <Button
-            variant={'none'}
-            className="text-schemafy-dark-gray min-w-[10rem] text-[1rem]"
-          >
-            Contact
-          </Button>
-        </div>
-        <p className="self-stretch text-center text-schemafy-dark-gray">
-          © 2024 Schemafy. All rights reserved.
-        </p>
-      </div>
+    <footer className="w-full flex justify-center py-10">
+      <p className="self-stretch text-center text-schemafy-dark-gray">
+        © 2025 Schemafy. All rights reserved.
+      </p>
     </footer>
   );
 };
