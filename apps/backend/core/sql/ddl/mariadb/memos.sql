15bb5b5d09b776e2b914d218164266ec8e83a58f
cjeongmin <cjeongmin27@gmail.com>
2025-11-22 21:10:26 +0900

    feat(#97): add memo and memo_comments tables

---
+++ apps/backend/core/sql/ddl/mariadb/memos.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS memos (
+    id          CHAR(26)  NOT NULL,
+    schema_id   CHAR(26)  NOT NULL,
+    author_id   CHAR(26)  NOT NULL,
+    positions   TEXT      NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at  TIMESTAMP NULL,
+    CONSTRAINT pk_memos PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
