048034572a2c713bceac28f621b5aa507f02c234
Choi Jeongmin <cjeongmin27@gmail.com>
2025-11-24 19:52:05 +0900

    feat(#77): add DB vendor and data type management (#90)

--- apps/backend/core/sql/ddl/h2/db_schemas.sql
+++ apps/backend/core/sql/ddl/h2/db_schemas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS db_schemas (
     id              CHAR(26)     NOT NULL,
     project_id      CHAR(26)     NOT NULL,
-    db_vendor_id    VARCHAR(64)  NOT NULL,
+    db_vendor_id    VARCHAR(255) NOT NULL,
     name            VARCHAR(255) NOT NULL,
     charset         VARCHAR(64)  NULL,
     collation       VARCHAR(64)  NULL,
