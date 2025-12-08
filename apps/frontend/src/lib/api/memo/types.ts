2ff7a73f9bbee84c54da027b787fd15b8e03f289
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2025-12-08 22:43:23 +0900

    feat(#103): implement memo api

---
+++ apps/frontend/src/lib/api/memo/types.ts
@@ -0,0 +1,36 @@
+export type MemoComment = {
+  id: string;
+  memoId: string;
+  authorId: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Memo = {
+  id: string;
+  schemaId: string;
+  authorId: string;
+  positions: string;
+  createdAt: string;
+  updatedAt: string;
+  comments?: MemoComment[];
+};
+
+export type CreateMemoRequest = {
+  schemaId: string;
+  positions: string;
+  body: string;
+};
+
+export type UpdateMemoRequest = {
+  positions: string;
+};
+
+export type CreateMemoCommentRequest = {
+  body: string;
+};
+
+export type UpdateMemoCommentRequest = {
+  body: string;
+};
