0f148aaa8c74bbc6e1c97c55dc02b8b85476dff1
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-01-07 14:25:41 +0900

    feat(#171): nest proxy server project setting

---
+++ apps/proxy-server/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  await app.listen(4000);
+  console.log(`Proxy server is running on port 4000`);
+}
+
+void bootstrap();
