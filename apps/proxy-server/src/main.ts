06915b8a9fbde6e355a9246671d084ec8b109ec4
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-01-07 14:27:08 +0900

    fix(#171): CORS enable port fix

--- apps/proxy-server/src/main.ts
+++ apps/proxy-server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3001',
     credentials: true,
   });
 
