41bd9428c7cde63da181abaadd422c54083d0b5e
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-01-15 01:48:36 +0900

    feat(#184): delete app service, controller

--- apps/bff/src/app.module.ts
+++ apps/bff/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MemoModule } from './memo/memo.module';
 
 @Module({
   imports: [MemoModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
