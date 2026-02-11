4e53083fbc156afcad3ef7cbf3bc5a2a13873a96
he2e2 <dddd6368@naver.com>
2026-02-11 15:27:22 +0900

    feat(#215): add schema service and controller

--- apps/bff/src/erd/erd.controller.ts
+++ apps/bff/src/erd/erd.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ErdService } from './erd.service';
+import type {
+  ChangeSchemaNameRequest,
+  CreateSchemaRequest,
+} from './erd.types';
 
 @Controller('api/v1.0')
 export class ErdController {
   constructor(private readonly erdService: ErdService) {}
+
+  @Post('schemas')
+  async createSchema(
+    @Body() data: CreateSchemaRequest,
+    @Headers('authorization') authHeader: string,
+  ) {
+    return this.erdService.createSchema(data, authHeader);
+  }
+
+  @Get('schemas/:schemaId')
+  async getSchema(
+    @Param('schemaId') schemaId: string,
+    @Headers('authorization') authHeader: string,
+  ) {
+    return this.erdService.getSchema(schemaId, authHeader);
+  }
+
+  @Patch('schemas/:schemaId/name')
+  async changeSchemaName(
+    @Param('schemaId') schemaId: string,
+    @Body() data: ChangeSchemaNameRequest,
+    @Headers('authorization') authHeader: string,
+  ) {
+    return this.erdService.changeSchemaName(schemaId, data, authHeader);
+  }
+
+  @Delete('schemas/:schemaId')
+  async deleteSchema(
+    @Param('schemaId') schemaId: string,
+    @Headers('authorization') authHeader: string,
+  ) {
+    return this.erdService.deleteSchema(schemaId, authHeader);
+  }
 }
