787d8774e598e93d2727380f0ba0655c70eb69e8
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-03-17 01:33:24 +0900

    feat(#285): move collaboration component to features/collaboration

--- apps/frontend/src/pages/CanvasPage.tsx
+++ apps/frontend/src/pages/CanvasPage.tsx
@@ -1,30 +1,24 @@
 import { observer } from 'mobx-react-lite';
-import {
-  ReactFlow,
-  MiniMap,
-  Background,
-  BackgroundVariant,
-  ConnectionMode,
-} from '@xyflow/react';
+import { Background, BackgroundVariant, ConnectionMode, MiniMap, ReactFlow, } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import {
-  TableNode,
-  RelationshipMarker,
-  Toolbar,
-  RelationshipEditor,
-  CustomControls,
-  TablePreview,
-  CustomSmoothStepEdge,
   CustomConnectionLine,
+  CustomControls,
+  CustomSmoothStepEdge,
   FloatingButtons,
+  RelationshipEditor,
+  RelationshipMarker,
   SchemaSelector,
-  TempMemoPreview,
-  useCanvasController,
   SelectedSchemaProvider,
+  TableNode,
+  TablePreview,
+  TempMemoPreview,
+  Toolbar,
+  useCanvasController,
 } from '@/features/drawing';
 import { Memo, MemoPreview } from '@/features/memo/components';
 import { MemoProvider } from '@/features/memo/context';
-import { ChatOverlay, ChatInput } from '@/components/Collaboration';
+import { ChatInput, ChatOverlay } from '@/features/collaboration/components';
 
 const NODE_TYPES = {
   table: TableNode,
@@ -45,8 +39,8 @@ const CanvasContent = observer(() => {
       chatInputPosition,
       selectedRelationship,
     },
-    setter: { setRelationshipConfig, setActiveTool, setSelectedRelationship },
-    data: { nodes, relationships },
+    setter: {setRelationshipConfig, setActiveTool, setSelectedRelationship},
+    data: {nodes, relationships},
     handlers: {
       handleNodesChange,
       handleNodeDragStop,
@@ -72,7 +66,7 @@ const CanvasContent = observer(() => {
 
   return (
     <>
-      <RelationshipMarker />
+      <RelationshipMarker/>
       <div className="flex flex-1">
         <Toolbar
           setActiveTool={setActiveTool}
@@ -83,7 +77,7 @@ const CanvasContent = observer(() => {
 
         <div className="flex-1 bg-schemafy-secondary relative">
           <div className="absolute top-4 right-4 z-10">
-            <SchemaSelector />
+            <SchemaSelector/>
           </div>
 
           <div
@@ -113,7 +107,7 @@ const CanvasContent = observer(() => {
               nodeTypes={NODE_TYPES}
               edgeTypes={EDGE_TYPES}
               connectionLineComponent={CustomConnectionLine}
-              proOptions={{ hideAttribution: true }}
+              proOptions={{hideAttribution: true}}
               connectionMode={ConnectionMode.Loose}
               fitView={false}
               minZoom={0.1}
@@ -135,14 +129,14 @@ const CanvasContent = observer(() => {
                 zoomable
                 pannable
               />
-              <CustomControls />
-              <Background variant={BackgroundVariant.Dots} />
+              <CustomControls/>
+              <Background variant={BackgroundVariant.Dots}/>
 
               {activeTool === 'table' && (
-                <TablePreview mousePosition={mousePosition} />
+                <TablePreview mousePosition={mousePosition}/>
               )}
               {activeTool === 'memo' && (
-                <MemoPreview mousePosition={mousePosition} />
+                <MemoPreview mousePosition={mousePosition}/>
               )}
             </ReactFlow>
           </div>
@@ -175,8 +169,8 @@ const CanvasContent = observer(() => {
           )}
         </div>
       </div>
-      <FloatingButtons />
-      <ChatOverlay />
+      <FloatingButtons/>
+      <ChatOverlay/>
     </>
   );
 });
@@ -187,7 +181,7 @@ export const CanvasPage = () => {
   return (
     <SelectedSchemaProvider projectId={projectId}>
       <MemoProvider>
-        <CanvasContent />
+        <CanvasContent/>
       </MemoProvider>
     </SelectedSchemaProvider>
   );
