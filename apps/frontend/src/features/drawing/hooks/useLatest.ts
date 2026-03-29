9361c255d08b075394d59175726e14c5f342687b
he2e2 <dddd6368@naver.com>
2026-03-29 17:54:43 +0900

    refactor(#278): remove useEffect from useLatest

--- apps/frontend/src/features/drawing/hooks/useLatest.ts
+++ apps/frontend/src/features/drawing/hooks/useLatest.ts
@@ -1,9 +1,7 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 export const useLatest = <T>(value: T) => {
   const ref = useRef(value);
-  useEffect(() => {
-    ref.current = value;
-  });
+  ref.current = value;
   return ref;
 };
