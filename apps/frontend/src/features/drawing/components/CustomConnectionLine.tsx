bd298c0d1b44f3a54071dc884e0c407fb2c2c508
he2e2 <dddd6368@naver.com>
2026-06-05 22:55:08 +0900

    Revert "feat(#378): update connection preview anchors"
    This reverts commit 3ec244610ef12580a3068f969e0256586c009690.

--- apps/frontend/src/features/drawing/components/CustomConnectionLine.tsx
+++ apps/frontend/src/features/drawing/components/CustomConnectionLine.tsx
@@ -1,10 +1,6 @@
 import { type ConnectionLineComponentProps } from '@xyflow/react';
 import type { Point } from '../types';
 import {
-  getFloatingAnchorPoint,
-  getInternalNodeRect,
-} from '../utils/anchorGeometry';
-import {
   isHorizontalPosition,
   getDefaultControlPoints,
   buildCrossDirectionPath,
@@ -18,38 +14,13 @@ export const CustomConnectionLine = ({
   toY,
   fromPosition,
   toPosition,
-  fromNode,
-  toNode,
 }: ConnectionLineComponentProps) => {
-  const fromRect = getInternalNodeRect(fromNode);
-  const toRect = getInternalNodeRect(toNode ?? undefined);
-  const cursorRect = {
-    x: toX,
-    y: toY,
-    width: 1,
-    height: 1,
-  };
-
-  const sourceAnchor = fromRect
-    ? getFloatingAnchorPoint(fromRect, toRect ?? cursorRect)
-    : null;
-  const targetAnchor =
-    fromRect && toRect ? getFloatingAnchorPoint(toRect, fromRect) : null;
-
-  const effectiveFromPosition = sourceAnchor?.position ?? fromPosition;
-  const effectiveToPosition = targetAnchor?.position ?? toPosition;
-  const sourceIsHorizontal = isHorizontalPosition(effectiveFromPosition);
-  const targetIsHorizontal = isHorizontalPosition(effectiveToPosition);
+  const sourceIsHorizontal = isHorizontalPosition(fromPosition);
+  const targetIsHorizontal = isHorizontalPosition(toPosition);
   const isCrossDirection = sourceIsHorizontal !== targetIsHorizontal;
 
-  const source: Point = {
-    x: sourceAnchor?.point.x ?? fromX,
-    y: sourceAnchor?.point.y ?? fromY,
-  };
-  const target: Point = {
-    x: targetAnchor?.point.x ?? toX,
-    y: targetAnchor?.point.y ?? toY,
-  };
+  const source: Point = { x: fromX, y: fromY };
+  const target: Point = { x: toX, y: toY };
 
   const controlPoints = getDefaultControlPoints(
     source,
